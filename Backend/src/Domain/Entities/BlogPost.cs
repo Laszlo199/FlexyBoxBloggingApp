8011b74338d5e1fc90e2122b2bb1bf9a1d0089fd
Laszlo <70133057+Laszlo199@users.noreply.github.com>
2024-10-02 21:00:06 +0200

    Implement entities
    - User
    - BlogPost

---
+++ Backend/src/Domain/Entities/BlogPost.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain.Entities
+{
+    public class BlogPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastUpdatedAt { get; set; }
+        public int AuthorId { get; set; }
+
+    }
+}
