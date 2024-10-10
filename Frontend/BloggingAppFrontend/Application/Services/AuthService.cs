0012ee6ba11de4fb0cee4b15bb747a34c546324a
Laszlo <70133057+Laszlo199@users.noreply.github.com>
2024-10-10 17:08:39 +0200

    Update AuthService.cs

--- Frontend/BloggingAppFrontend/Application/Services/AuthService.cs
+++ Frontend/BloggingAppFrontend/Application/Services/AuthService.cs
@@ -1,6 +1,78 @@
-﻿namespace BloggingAppFrontend.Application.Services
+﻿using Blazored.LocalStorage;
+using BloggingAppFrontend.Application.Dtos.AuthDtos;
+using Microsoft.AspNetCore.Components;
+
+namespace BloggingAppFrontend.Application.Services
 {
-    public class AuthService
+    public interface IAuthService
     {
+        TokenDto? TokenDto { get; }
+        Task Initialize();
+        Task<bool> Register(LoginDto loginDto);
+        Task<bool> Login(LoginDto loginDto);
+        Task Logout();
+    }
+
+    public class AuthService : IAuthService
+    {
+        private IHttpService _httpService;
+        private NavigationManager _navigationManager;
+        private ILocalStorageService _localStorageService;
+        public TokenDto? TokenDto { get; private set; }
+
+        public AuthService(
+            IHttpService httpService,
+            NavigationManager navigationManager,
+            ILocalStorageService localStorageService
+        )
+        {
+            _httpService = httpService;
+            _navigationManager = navigationManager;
+            _localStorageService = localStorageService;
+        }
+
+        public async Task Initialize()
+        {
+            TokenDto = await _localStorageService.GetItemAsync<TokenDto>("tokenDto");
+        }
+
+        public async Task<bool> Register(LoginDto loginDto)
+        {
+            var loginDtoCopy = new LoginDto
+            {
+                Email = loginDto.Email,
+                Password = loginDto.Password,
+            };
+            TokenDto = await _httpService.Post<TokenDto>("/api/Auth/Register", loginDtoCopy);
+            if (TokenDto is null)
+            {
+                return false;
+            }
+            await _localStorageService.SetItemAsync("tokenDto", TokenDto);
+            return true;
+        }
+
+        public async Task<bool> Login(LoginDto loginDto)
+        {
+            var loginDtoCopy = new LoginDto
+            {
+                Email = loginDto.Email,
+                Password = loginDto.Password,
+            };
+            TokenDto = await _httpService.Post<TokenDto>("/api/Auth/login", loginDtoCopy);
+            if (TokenDto is null)
+            {
+                return false;
+            }
+            await _localStorageService.SetItemAsync("tokenDto", TokenDto);
+            return true;
+        }
+
+        public async Task Logout()
+        {
+            TokenDto = null;
+            await _localStorageService.RemoveItemAsync("tokenDto");
+            _navigationManager.NavigateTo("login");
+        }
     }
 }
