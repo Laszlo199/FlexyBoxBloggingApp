31cdbb79830bc0cf6e58a1a61a59206b71dc4208
Laszlo <70133057+Laszlo199@users.noreply.github.com>
2024-10-10 16:31:24 +0200

    delelte frontend

--- Frontend/src/BloggingAppFrontend/Application/Dtos/AuthDtos/RegisterDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BloggingAppFrontend.Application.Dtos.AuthDtos
-{
-    public class RegisterDto
-    {
-        public required string Email { get; set; }
-        public required string Password { get; set; }
-        public required string Username { get; set; }
-    }
-}
