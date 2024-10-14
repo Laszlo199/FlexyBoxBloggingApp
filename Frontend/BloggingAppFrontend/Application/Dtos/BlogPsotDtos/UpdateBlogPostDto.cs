e4e9089803fd6b1300cd07cf12e3bd74fdb700aa
Laszlo <70133057+Laszlo199@users.noreply.github.com>
2024-10-14 21:31:25 +0200

    validation and toast more and more

--- Frontend/BloggingAppFrontend/Application/Dtos/BlogPsotDtos/UpdateBlogPostDto.cs
+++ Frontend/BloggingAppFrontend/Application/Dtos/BlogPsotDtos/UpdateBlogPostDto.cs
@@ -1,9 +1,14 @@
-﻿namespace BloggingAppFrontend.Application.Dtos.BlogPsotDtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BloggingAppFrontend.Application.Dtos.BlogPsotDtos
 {
     public class UpdateBlogPostDto
     {
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Content { get; set; }
+        [Required]
         public List<int> CategoryIds { get; set; } = new List<int>();
     }
 }
